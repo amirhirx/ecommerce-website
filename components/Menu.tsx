05e7f3ff323adc34cc137d348aac7ad8a56b4ba9
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-11-08 22:05:53 +0330

    Add search icon from react-icons

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { IoSearchOutline } from "react-icons/io5"
 
 export default function Menu() {
     return (
@@ -6,11 +7,12 @@ export default function Menu() {
             <div className="flex items-center gap-4 w-6/12">
                 <Image src={"/logo.svg"} alt="Logo" width={56} height={56} />
                 <span className="font-black text-xl">فروشگاه</span>
-                <div className="bg-gray-100 p-2 rounded w-full">
+                <div className="bg-gray-100 p-2 rounded w-full flex items-center gap-2 text-gray-600">
+                    <IoSearchOutline className="text-2xl" />
                     <input
                         type="text"
                         placeholder="جستجو"
-                        className="outline-none"
+                        className="outline-none w-full"
                     />
                 </div>
             </div>
