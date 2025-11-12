a3576221bf788350d371840dc9038e588409298e
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-11-12 11:00:00 +0330

    Add Footer to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import "./globals.css"
 import Menu from "@/components/Menu"
+import Footer from "@/components/Footer"
 
 export const metadata: Metadata = {
     title: "Ecommerce Website",
@@ -17,6 +18,7 @@ export default function RootLayout({
             <body className={`bg-gray-200 antialiased`}>
                 <Menu />
                 {children}
+                <Footer />
             </body>
         </html>
     )
