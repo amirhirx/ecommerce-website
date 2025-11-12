de5f0d0245c496201f029148055bdc9bd4093c7c
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-11-12 11:01:07 +0330

    Add ProductsCarousel to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import ProductsCarousel from "@/components/ProductsCarousel"
 import Slider from "@/components/Slider"
 
 export default function Home() {
     return (
         <div>
             <Slider />
+            <div className="px-6 py-2">
+                <ProductsCarousel />
+            </div>
         </div>
     )
 }
