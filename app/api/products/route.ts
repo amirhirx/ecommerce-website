8ca25cb704b6bb90ba2b90e47cebf9170c13e794
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-11-14 14:13:09 +0330

    Response error if an error happend

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -2,5 +2,9 @@ import { NextResponse } from "next/server"
 import products from "@/data.json"
 
 export async function GET() {
-    return NextResponse.json({ products }, { status: 200 })
+    try {
+        return NextResponse.json({ products }, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ error: error }, { status: 500 })
+    }
 }
