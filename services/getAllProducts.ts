31e863bcf3ba0243d811579f591f5c3d867cfc83
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-11-14 15:02:12 +0330

    Create getAllProducts for get all products from API

---
+++ services/getAllProducts.ts
@@ -0,0 +1,7 @@
+const url = "http://localhost:3000/api/products"
+
+export async function getAllProducts() {
+    const res = await fetch(url)
+    const { products } = await res.json()
+    return products
+}
