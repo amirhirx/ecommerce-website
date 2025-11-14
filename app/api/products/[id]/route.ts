8da63dd7b3abaa079701a83efa4d96e60cd78e18
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-11-14 14:45:46 +0330

    Create an API for get product data with id

---
+++ app/api/products/[id]/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server"
+import productsJSON from "@/data.json"
+import { IProduct } from "@/types/Product"
+
+export async function GET(request: Request, { params }: never) {
+    const { id } = await params
+    const products: IProduct[] = productsJSON.products
+    const product = products.find((product) => {
+        return product.id == id
+    })
+
+    try {
+        return NextResponse.json({ product }, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ error: error }, { status: 500 })
+    }
+}
