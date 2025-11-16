6da0dacc954672674c13da11b79165a964cae30c
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-11-16 16:34:15 +0330

    Fix ProductCard component style

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -18,11 +18,11 @@ export default function ProductCard({
         <Link href={`/products/${slug}`}>
             <div className="md:w-60 w-40 p-4 bg-white rounded-lg shadow-lg flex flex-col gap-4">
                 <img
-                    className="w-full rounded-lg shadow"
+                    className="w-full md:h-52 h-32 rounded-lg shadow object-cover"
                     alt="Thumbnail"
                     src={image}
                 />
-                <h3 className="text-md font-medium line-clamp-2">{title}</h3>
+                <h3 className="text-sm font-medium line-clamp-2">{title}</h3>
                 <p className="text-left font-bold">
                     <span className="text-lg">{price}</span>
                     <span className="mr-1">تومان</span>
