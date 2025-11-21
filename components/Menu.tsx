91234fac7ea0ecc4d7bbd7e07911c33a63f8d33b
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-11-21 20:01:05 +0330

    Make menu responsive

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,70 +1,98 @@
 import Image from "next/image"
 import Link from "next/link"
-import { IoSearchOutline } from "react-icons/io5"
 import { PiShoppingCartSimple } from "react-icons/pi"
 import { TbLogin } from "react-icons/tb"
 import { GoBell } from "react-icons/go"
+import { GoHomeFill } from "react-icons/go"
+import { BiSolidCategory } from "react-icons/bi"
+import { IoMdMore } from "react-icons/io"
+import MenuSearch from "./MenuSearch"
 
 export default function Menu() {
     return (
-        <div className="bg-white shadow-md">
-            <div className=" py-2 px-4 flex items-center justify-between">
-                <div className="flex items-center gap-4 w-6/12">
+        <>
+            <div className="bg-white shadow-md md:block hidden">
+                <div className=" py-2 px-4 flex items-center justify-between">
+                    <div className="flex items-center gap-4 w-6/12">
+                        <Image
+                            src={"/logo.svg"}
+                            alt="Logo"
+                            width={56}
+                            height={56}
+                        />
+                        <span className="font-black text-xl">فروشگاه</span>
+                        <MenuSearch />
+                    </div>
+                    <div className="flex gap-4 items-center ">
+                        <GoBell className="text-2xl text-gray-600" />
+                        <Link
+                            href={"/"}
+                            className="flex gap-2 items-center border border-gray-200  rounded-lg px-4 py-2"
+                        >
+                            <TbLogin className="text-2xl text-gray-700" />
+                            <span className="text-gray-700 text-sm font-bold">
+                                ورود | ثبت نام
+                            </span>
+                        </Link>
+                        <PiShoppingCartSimple className="text-2xl text-gray-600" />
+                    </div>
+                </div>
+                <div className="p-2">
+                    <nav className="flex gap-2">
+                        <span>دسته بندی کالاها</span>
+                        <ul className="flex gap-4 text-gray-600">
+                            <li>
+                                <Link href={"/"}>لینک ۱</Link>
+                            </li>
+                            <li>
+                                <Link href={"/"}>لینک ۲</Link>
+                            </li>
+                            <li>
+                                <Link href={"/"}>لینک ۳</Link>
+                            </li>
+                            <li>
+                                <Link href={"/"}>لینک ۴</Link>
+                            </li>
+                            <li>
+                                <Link href={"/"}>لینک ۵</Link>
+                            </li>
+                            <li>
+                                <Link href={"/"}>لینک ۶</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div className="md:hidden">
+                <div className="bg-white flex justify-between items-center p-4 gap-4 shadow-md">
                     <Image
                         src={"/logo.svg"}
                         alt="Logo"
-                        width={56}
-                        height={56}
+                        width={40}
+                        height={40}
                     />
-                    <span className="font-black text-xl">فروشگاه</span>
-                    <div className="bg-gray-100 p-2 rounded w-full flex items-center gap-2 text-gray-600">
-                        <IoSearchOutline className="text-2xl" />
-                        <input
-                            type="text"
-                            placeholder="جستجو"
-                            className="outline-none w-full"
-                        />
+                    <MenuSearch />
+                    <IoMdMore className="w-10 h-10 text-gray-600" />
+                </div>
+                <div className="w-full flex gap-4 fixed bottom-0 bg-white p-4 items-center justify-around text-gray-600 shadow-md">
+                    <div className="flex flex-col items-center gap-2">
+                        <GoHomeFill className="w-6 h-6" />
+                        <span className="text-xs">صفحه اصلی</span>
+                    </div>
+                    <div className="flex flex-col items-center gap-2">
+                        <BiSolidCategory className="w-6 h-6" />
+                        <span className="text-xs">دسته بندی ها</span>
+                    </div>
+                    <div className="flex flex-col items-center gap-2">
+                        <PiShoppingCartSimple className="w-6 h-6" />
+                        <span className="text-xs">سبد خرید</span>
+                    </div>
+                    <div className="flex flex-col items-center gap-2">
+                        <TbLogin className="w-6 h-6" />
+                        <span className="text-xs">ورود</span>
                     </div>
                 </div>
-                <div className="flex gap-4 items-center ">
-                    <GoBell className="text-2xl text-gray-600" />
-                    <Link
-                        href={"/"}
-                        className="flex gap-2 items-center border border-gray-200  rounded-lg px-4 py-2"
-                    >
-                        <TbLogin className="text-2xl text-gray-700" />
-                        <span className="text-gray-700 text-sm font-bold">
-                            ورود | ثبت نام
-                        </span>
-                    </Link>
-                    <PiShoppingCartSimple className="text-2xl text-gray-600" />
-                </div>
             </div>
-            <div className="p-2">
-                <nav className="flex gap-2">
-                    <span>دسته بندی کالاها</span>
-                    <ul className="flex gap-4 text-gray-600">
-                        <li>
-                            <Link href={"/"}>لینک ۱</Link>
-                        </li>
-                        <li>
-                            <Link href={"/"}>لینک ۲</Link>
-                        </li>
-                        <li>
-                            <Link href={"/"}>لینک ۳</Link>
-                        </li>
-                        <li>
-                            <Link href={"/"}>لینک ۴</Link>
-                        </li>
-                        <li>
-                            <Link href={"/"}>لینک ۵</Link>
-                        </li>
-                        <li>
-                            <Link href={"/"}>لینک ۶</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
+        </>
     )
 }
---
+++ components/MenuSearch.tsx
@@ -0,0 +1,14 @@
+import { IoSearchOutline } from "react-icons/io5"
+
+export default function MenuSearch() {
+    return (
+        <div className="flex bg-gray-100 p-2 rounded w-full items-center gap-2 text-gray-600">
+            <IoSearchOutline className="text-2xl" />
+            <input
+                type="text"
+                placeholder="جستجو"
+                className="outline-none w-full"
+            />
+        </div>
+    )
+}
