7974297a198413e37c86c412bd12ac20305bd55f
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-11-25 16:26:52 +0330

    Create addItem for add products to cart

--- contexts/CartContext.tsx
+++ contexts/CartContext.tsx
@@ -12,13 +12,33 @@ export interface ICartProduct {
 
 export interface ICartContextProvider {
     cart: ICartProduct[]
+    addItem: (id: number) => void
 }
 
 function CartContextProvider({ children }: { children: React.ReactNode }) {
     const [cart, setCart] = useState<ICartProduct[]>([] as ICartProduct[])
 
+    const addItem = (id: number) => {
+        const itemInCart = cart.find((item) => item.id == id)
+        if (itemInCart) {
+            setCart((prevCart) => {
+                return prevCart.map((item) => {
+                    if (item.id == itemInCart.id) {
+                        return { ...item, qty: item.qty++ }
+                    } else {
+                        return item
+                    }
+                })
+            })
+        } else {
+            setCart((prev) => [...prev, { id: id, qty: 1 }])
+        }
+    }
+
     return (
-        <CartContext.Provider value={{ cart }}>{children}</CartContext.Provider>
+        <CartContext.Provider value={{ cart, addItem }}>
+            {children}
+        </CartContext.Provider>
     )
 }
 
