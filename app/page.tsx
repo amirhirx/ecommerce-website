00153d8e34a9d627fedc7a5349ef37a57972ec26
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-12-09 20:50:04 +0330

    Remove BrandsList, Slider component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,3 @@
-import BrandsList from "@/components/BrandsList"
-import ProductsCarousel from "@/components/ProductsCarousel"
-import Slider from "@/components/Slider"
-
 export default function Home() {
-    return (
-        <div>
-            <Slider />
-            <div className="px-6 py-2">
-                <ProductsCarousel />
-            </div>
-            <div className="p-4">
-                <BrandsList />
-            </div>
-        </div>
-    )
+    return <div></div>
 }
