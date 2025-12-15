003047ace3f11a2173ebfd4efa631db1b041ea29
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-12-15 22:11:36 +0330

    Create a full-width slider for Hero component & Add it to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+"use client"
+import Hero from "@/components/Hero"
+
 export default function Home() {
-    return <div></div>
+    return (
+        <main>
+            <Hero />
+        </main>
+    )
 }
