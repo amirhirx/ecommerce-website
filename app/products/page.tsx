a85f0b94b9f58c81915e37f7a22f0f29f8daca71
AmirHossein Hosseinian <amirhirx@gmail.com>
2026-01-02 22:00:50 +0330

    Refactor products page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -13,29 +13,25 @@ export const metadata: Metadata = {
 export default async function Products() {
     const { products } = await getAllProducts()
     return (
-        <div className="p-4">
+        <div className="w-[97.5%] mx-auto">
             <h1 className="text-2xl font-black my-2">محصولات</h1>
-            <div className="flex flex-col-reverse md:flex-row gap-4">
-                <div className="md:w-2/12 w-full">
-                    <ProductsFilter />
-                </div>
-                <div className="md:w-10/12 w-full">
-                    <ProductsContainer>
-                        {products.map(
-                            ({ id, title, price, images, slug }: IProduct) => {
-                                return (
-                                    <ProductCard
-                                        key={id}
-                                        title={title}
-                                        price={price}
-                                        image={images[0]}
-                                        slug={slug}
-                                    />
-                                )
-                            }
-                        )}
-                    </ProductsContainer>
-                </div>
+            <div className="md:flex md:items-start md:gap-6">
+                <ProductsFilter />
+                <ProductsContainer>
+                    {products.map(
+                        ({ id, title, price, images, slug }: IProduct) => {
+                            return (
+                                <ProductCard
+                                    key={id}
+                                    title={title}
+                                    price={price}
+                                    image={images[0]}
+                                    slug={slug}
+                                />
+                            )
+                        }
+                    )}
+                </ProductsContainer>
             </div>
         </div>
     )
