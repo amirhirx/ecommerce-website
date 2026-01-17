876402240300cf7975bc04f0662b3c3ccfa3f648
AmirHossein Hosseinian <amirhirx@gmail.com>
2026-01-17 22:05:40 +0330

    Update ProductCard styles

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -12,20 +12,20 @@ export default function ProductCard({
     const priceAfterDiscount = price - Math.ceil((price * discount) / 100)
     return (
         <Link href={`/products/${slug}`}>
-            <div className="md:w-60 w-full p-4 bg-white rounded-lg shadow-lg flex flex-col gap-4 relative">
+            <div className="md:w-60 w-full p-4 bg-white rounded-lg shadow-lg border border-gray-200 flex flex-col gap-4 relative">
                 <img
                     className="w-full md:h-52 h-72 rounded-lg shadow object-cover"
                     alt="Thumbnail"
                     src={image}
                 />
-                <h3 className="text-sm font-medium line-clamp-2">{title}</h3>
+                <h3 className="md:text-sm font-medium line-clamp-1">{title}</h3>
                 <div className="flex items-center justify-between">
-                    <div className="font-semibold">
+                    <div className="text-lg md:text-sm font-semibold">
                         {discount ? priceAfterDiscount : price} تومان{" "}
                     </div>
                     {discount ? (
                         <>
-                            <div className="text-xs text-red-500 font-medium">
+                            <div className="md:text-xs text-red-500 font-medium">
                                 {price} تومان
                             </div>
                             <div className="absolute top-3 left-3 w-8 h-8 bg-red-500 text-white font-medium text-sm rounded-xl flex items-center justify-center">
