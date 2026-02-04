0e8827d18dae931a2deb4f6e996af3feeadc4415
AmirHossein Hosseinian <amirhirx@gmail.com>
2026-02-04 20:23:19 +0330

    Update getProductBySlug service

--- services/getProductBySlug.ts
+++ services/getProductBySlug.ts
@@ -1,5 +1,5 @@
 export async function getProductBySlug(slug: string) {
-    const url = `http://localhost:3000/api/products/${slug}`
+    const url = `http://localhost:3000/api/products/?slug=${slug}`
     const res = await fetch(url)
     const { product } = await res.json()
     return product
