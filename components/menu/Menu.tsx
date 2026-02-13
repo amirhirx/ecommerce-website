038081f93100c82c089d40212ed5a13654b1f6d3
AmirHossein Hosseinian <amirhirx@gmail.com>
2026-02-13 21:58:32 +0330

    Remove submenu from Menu component

--- components/menu/Menu.tsx
+++ components/menu/Menu.tsx
@@ -1,14 +1,10 @@
 import Link from "next/link"
-
 import { FiHome } from "react-icons/fi"
 import { BsShop } from "react-icons/bs"
 import { MdOutlineArticle } from "react-icons/md"
 import { IoInformationCircleOutline } from "react-icons/io5"
 import { TfiHeadphoneAlt } from "react-icons/tfi"
 import { FiUser } from "react-icons/fi"
-import { SlFire } from "react-icons/sl"
-import { FaHashtag } from "react-icons/fa6"
-import { HiOutlineMagnifyingGlass } from "react-icons/hi2"
 import MenuNavigation from "./MenuNavigation"
 import { INavigation } from "@/types/navigation"
 import CartIcon from "./CartIcon"
@@ -21,60 +17,21 @@ const navigations: INavigation[] = [
     { text: "تماس با ما", href: "/contact", icon: TfiHeadphoneAlt({}) },
 ]
 
-const topTags: string[] = ["لپ_تاپ_گیمینگ", "دانشجویی", "مدیریت_شرکت"]
-
 export default function Menu() {
     return (
-        <header className="w-full md:w-98/100 md:my-4 md:mx-auto bg-green-400 md:rounded-xl md:overflow-hidden shadow">
-            <div className="flex items-center justify-between bg-white p-4 md:p-6 md:rounded-br-3xl">
-                <div>
-                    <Link href={"/"}>
-                        <h1 className="text-lg font-black">وبسایت فروشگاهی</h1>
-                    </Link>
-                </div>
-                <MenuNavigation items={navigations} />
-                <div className="flex items-center gap-4">
-                    <Link href={"#"}>
-                        <FiUser size={22} />
-                    </Link>
-
-                    <CartIcon />
-                </div>
+        <header className="w-full md:w-98/100 md:my-1.5 mb-1.5 py-3.5 px-4 md:mx-auto md:rounded-xl md:overflow-hidden shadow flex items-center justify-between bg-white ">
+            <div>
+                <Link href={"/"}>
+                    <h1 className="text-lg font-black">وبسایت فروشگاهی</h1>
+                </Link>
             </div>
-            <div className="hidden md:flex items-center justify-between py-2 px-6">
-                <div>
-                    <ul className="flex item-center gap-4">
-                        <h2 className="font-bold flex items-center gap-2">
-                            <SlFire />
-                            محبوبترین ها
-                        </h2>
-                        {topTags.map((tag, index) => {
-                            return (
-                                <li key={`tag-${index}`}>
-                                    <Link
-                                        href={"#"}
-                                        className="flex items-center gap-1 font-medium"
-                                    >
-                                        <FaHashtag />
-                                        {tag}
-                                    </Link>
-                                </li>
-                            )
-                        })}
-                    </ul>
-                </div>
-                <div>
-                    <form className="flex item-center gap-2">
-                        <input
-                            type="text"
-                            placeholder="جستجوی محصولات..."
-                            className="bg-white rounded-lg outline-none p-1 text-sm"
-                        />
-                        <button className="bg-green-600 rounded-lg p-1 px-1.5">
-                            <HiOutlineMagnifyingGlass className="text-white" />
-                        </button>
-                    </form>
-                </div>
+            <MenuNavigation items={navigations} />
+            <div className="flex items-center gap-4">
+                <Link href={"#"}>
+                    <FiUser size={22} />
+                </Link>
+
+                <CartIcon />
             </div>
         </header>
     )
