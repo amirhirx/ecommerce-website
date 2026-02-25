1d7b49cb09e3c26b4b85f651423d1bc547b34a9b
AmirHossein Hosseinian <amirhirx@gmail.com>
2026-02-25 22:01:42 +0330

    Update styles & Calculace the discounted & final price

--- components/cart/Checkout.tsx
+++ components/cart/Checkout.tsx
@@ -1,27 +1,43 @@
-import { CartContext } from "@/contexts/CartContext"
 import { IProductInCart } from "@/types/ProductInCart"
 import { formatPrice } from "@/utils/formatPrice"
-import { useContext, useEffect, useState } from "react"
+import { useState, useEffect } from "react"
 
 export function Checkout({ products }: { products: IProductInCart[] }) {
-    const { cart } = useContext(CartContext)
-    const [totalPrice, setTotalPrice] = useState<number>(0)
+    const [prices, setPrices] = useState<{
+        total: number
+        discounted: number
+    }>({ total: 0, discounted: 0 })
 
     useEffect(() => {
-        const setTotalPriceToZero = () => setTotalPrice(0)
-        setTotalPriceToZero()
-        products.map(({ product, qty }) => {
-            setTotalPrice((prev) => prev + product.price * qty)
+        let total = 0
+        let discounted = 0
+
+        products.forEach(({ product, qty }) => {
+            const { price, discount } = product
+            total += price * qty
+            discounted += ((price * discount) / 100) * qty
         })
-    }, [cart, products])
+
+        const updatePrices = () => setPrices({ discounted, total })
+        updatePrices()
+    }, [products])
 
     return (
         <div className="md:w-3/12 w-full h-max bg-white p-4 rounded-lg">
             <h2 className="text-xl font-extrabold">مجموع</h2>
             <ul className="space-y-2 py-4 font-medium">
-                <li>قیمت کل: {formatPrice(totalPrice)}</li>
-                <li>تخفیف: {formatPrice(0)}</li>
-                <li>قیمت نهایی: {formatPrice(0)}</li>
+                <li className="flex items-center gap-2">
+                    <span className="font-bold">قیمت کل:</span>
+                    <span>{formatPrice(prices.total)}</span>
+                </li>
+                <li className="flex items-center gap-2">
+                    <span className="font-bold">تخفیف:</span>
+                    <span>{formatPrice(prices.discounted)}</span>
+                </li>
+                <li className="flex items-center gap-2">
+                    <span className="font-bold">قیمت نهایی:</span>
+                    <span>{formatPrice(prices.total - prices.discounted)}</span>
+                </li>
             </ul>
             <button className="w-full bg-green-400 text-white font-bold py-1.5 rounded-lg cursor-pointer">
                 پرداخت
